e819d9e8133f835d4012eef24c1a97fb60088c8d
leoxxydee <104421466+leoxxydee@users.noreply.github.com>
2023-03-09 15:39:27 +0200

    scripture memorizer
    Signed-off-by: leoxxydee <104421466+leoxxydee@users.noreply.github.com>

---
+++ prove/Develop03/Scripture.cs
@@ -0,0 +1,171 @@
+using System;
+using System.Collections.Generic;
+
+namespace Namespace
+class Scripture
+{
+    static class Scripture
+    static void Main(string[] args)
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Develop05 World!");
+        }
+        Console.WriteLine("Hello World!");
+    }
+}
+
+namespace Scripture
+{
+    static class HelloWorld
+    {
+        static void Main()
+        {
+            ProgramManager manager = new();
+            ProgramManager.Run();
+        }
+    }
+
+    internal static class ProgramManager
+    {
+        public static void Run()
+        {
+            Scripture scripture;
+            Reference reference = new("Proverbs", "3", "5", "6");
+
+            Random random = new();
+            int randomNumber;
+
+            string quitString = "";
+            const string scriptureText = "Trust in the Lord with all thine heart; and lean not unto thine own understanding. In all thy ways acknowledge him, and he shall direct thy paths.";
+            string[] splitScriptureText = scriptureText.Split(" ");
+
+            List<Word> wordList = new();
+
+            foreach (string x in splitScriptureText)
+            {
+                wordList.Add(new Word(x));
+            }
+
+            scripture = new Scripture(reference, wordList);
+
+            while (quitString != "quit")
+            {
+                reference.PrintFullReference();
+                foreach (Word word in scripture.GetScriptureText())
+                {
+                    Console.Write(word.ShowWord() + " ");
+                }
+                Console.WriteLine("\n\nPress enter to continue or type 'quit' to finish:");
+                quitString = Console.ReadLine();
+                for (int i = 0; i < 3; i++)
+                {
+                    randomNumber = random.Next(0, scripture.GetScriptureText().Count);
+                    scripture.GetScriptureText()[randomNumber].SetHidden(true);
+                    Console.WriteLine(randomNumber);
+                }
+            }
+        }
+    }
+
+    class Scripture
+    {
+        private readonly List<Word> wordList;
+
+        public Scripture(Reference reference, List<Word> wordList)
+        {
+            Reference = reference;
+            this.wordList = wordList;
+        }
+
+        public Reference Reference { get; }
+
+        internal List<Word> GetScriptureText()
+        {
+            return wordList;
+        }
+    }
+
+    class Reference
+    {
+        private string book;
+        private readonly string chapter;
+        private string verse;
+        private string v;
+
+        public Reference(string bookParam, string chapterParam, string verseParam)
+        {
+            book = bookParam;
+            chapter = chapterParam;
+            verse = verseParam;
+        }
+
+        public Reference(string bookParam, string chapterParam, string verseParam, string v) : this(bookParam, chapterParam, verseParam)
+        {
+            this.v = v;
+        }
+
+        public void PrintFullReference()
+        {
+            Console.WriteLine($"{book} {chapter}:{verse}");
+        }
+    }
+
+    class Word
+    {
+        private string hiddenWord;
+        private bool isHidden;
+
+        public Word(string wordParam)
+        {
+            HiddenWord = "";
+            WordVal = wordParam;
+            foreach (char x in WordVal)
+            {
+                HiddenWord += "_";
+            }
+        }
+
+        public Word(string wordParam, bool isHiddenParam)
+        {
+            HiddenWord = "";
+            WordVal = wordParam;
+            IsHidden = isHiddenParam;
+
+            foreach (char x in WordVal)
+            {
+                HiddenWord += "_";
+            }
+        }
+
+        private string WordVal { get; set; }
+
+        public string HiddenWord { get => hiddenWord; private set => hiddenWord = value; }
+        public bool IsHidden { get => isHidden; private set => isHidden = value; }
+
+        public string ShowWord()
+        {
+            if (IsHidden == true)
+            {
+                return HiddenWord;
+            }
+            else
+            {
+                return WordVal;
+            }
+        }
+
+        public void SetHidden(bool isHiddenParam)
+        {
+            IsHidden = isHiddenParam;
+            if (IsHidden)
+            {
+                for (int i = 0; i < HiddenWord.Length; i++)
+                {
+                    HiddenWord = HiddenWord.Remove(i, 1).Insert(i, "_");
+                }
+            }
+        }
+    }
+}
+}
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,12 @@
-using System;
-
 class Program
 {
     static void Main(string[] args)
+
     {
         Console.WriteLine("Hello Sandbox World!");
+        Console.WriteLine("My name is Leonah");
+        Console.WriteLine("What's your name?");
+        string name = Console.ReadLine();
+        Console.WriteLine($"My name is {name}");
     }
 }
